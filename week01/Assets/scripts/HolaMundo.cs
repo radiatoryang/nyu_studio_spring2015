af02a8398cf348d3cbf7296c3dbfcece12dafd76
Robert Yang <yang.robert.w@gmail.com>
2015-02-04 16:46:20 -0500

    added week 1 (and week 2 stuff, which is actually in week 1... oops)

---
+++ week01/Assets/scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HolaMundo : MonoBehaviour {
+
+	// if you see a line with a "//" that means it is a COMMENT
+	// a COMMENT does not RUN as code
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ( "Bonjour Monde!" );
+		//GetComponent<Text>().text = "Au Revoir Monde";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log ( "Eat my shorts" );
+		GetComponent<Text>().text = Time.time.ToString ();
+	}
+}
