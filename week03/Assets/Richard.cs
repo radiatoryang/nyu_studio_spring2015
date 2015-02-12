8822bc59df7b0dc9f17b766af15a29b118f9ccdf
Robert Yang <yang.robert.w@gmail.com>
2015-02-12 00:04:30 -0500

    added week 3

---
+++ week03/Assets/Richard.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Richard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// move forward if I press W
+		// GetKeyDown = tapping, GetKey = holding
+		if ( Input.GetKey ( KeyCode.W ) ) 
+		{ // Time.deltaTime is the fraction of a second in between a frame
+	      // a value that gets bigger with low FPS, smaller with high FPS
+			transform.position += new Vector3( 0f, 0f, 10f ) * Time.deltaTime;
+		}
+
+		// FRAMERATE-DEPENDENT
+		// if I have more FPS, I go faster
+
+		// FRAMERATE-INDEPENDENT
+		// no matter what your framerate, you get same behavior
+
+
+
+		// move backward if I press S
+
+		// move left if I press A
+
+		// move right f I press D
+	}
+}
