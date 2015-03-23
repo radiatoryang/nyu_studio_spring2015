a4bf21d6fa56f882010720347a3584873151d7cd
Robert Yang <yang.robert.w@gmail.com>
2015-03-23 18:37:16 -0400

    WEEK 9: intro to while( ) loop and instantiation

---
+++ week09/Assets/scripts/CubeGod.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeGod : MonoBehaviour {
+
+	public Transform blueprint; // assign in Inspector
+	int cubeCountMax = 100;
+	float cubeSpawnRadius = 10f;
+
+	// Use this for initialization
+	void Start () {
+		// ITERATION
+		// ITERATION
+		// ITERATIOnnnn
+
+		// randomize system parameters
+		cubeCountMax = Random.Range (100, 1000);
+		cubeSpawnRadius = Random.Range ( 5f, 100f);
+
+		// how to instantiate a cube, simply
+		Instantiate ( 	blueprint, 
+		             	new Vector3( 3.14f, 0.62f, 1f ), 
+		             	Quaternion.Euler ( 0f, 180f, 0f ) 
+		            );
+
+		// how to instantiate multiple cubes
+		int cubeCount = 0;
+		while ( cubeCount < cubeCountMax ) {
+			MakeRandomCube ();
+			cubeCount = cubeCount + 1; // or "cubeCount++;" or "cubeCount += 1;"
+		}
+	}
+
+
+	void MakeRandomCube () {
+		Instantiate ( 	blueprint, 
+		             	Random.onUnitSphere * cubeSpawnRadius, 
+		             	Random.rotation
+		             );
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		// a simple restart / regenerate button
+		if ( Input.GetKeyDown (KeyCode.R) ) {
+			Application.LoadLevel ( Application.loadedLevel );
+		}
+
+		if ( Input.GetKey (KeyCode.Space) ) {
+			MakeRandomCube ();
+		}
+
+	}
+
+
+}
+
+
+
+
+
---
+++ week09/Assets/scripts/CubeSpin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpin : MonoBehaviour {
+
+	float spinSpeed;
+
+	// Use this for initialization
+	void Start () {
+		spinSpeed = Random.Range (90f, 270f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0f, Time.deltaTime * spinSpeed, 0f );
+	}
+}
