ccc3c4583bea4f7b7f25ec7c1a31c3957231e4f9
Robert Yang <yang.robert.w@gmail.com>
2015-03-30 15:58:58 -0400

    week 10: raycasting, detecting what player is clicking on, etc.

---
+++ week10/Assets/scripts/NPCCommand.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// we need this line to use Lists
+using System.Collections.Generic;
+
+// it doesn't really matter where you put this script as long as there's just one copy per scene
+// but I chose to put mine on my Main Camera, since it uses some camera functions
+public class NPCCommand : MonoBehaviour {
+
+	public NPCRaycast npcPrefab; // assign in inspector
+
+	// this is a List of references to NPC objects, specifically the NPC clones we instantiate
+	public List<NPCRaycast> allMyNpcs = new List<NPCRaycast>();
+
+	// Update is called once per frame
+	void Update () {
+		// first: find out where the player is clicking
+		// take player's mouse cursor position, and "project" out from camera's facing
+		Ray ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
+
+		// if we want "forensics" information about where the raycast hit, we need a blank variable
+		RaycastHit rayHit = new RaycastHit();
+
+		if ( Physics.Raycast ( ray, out rayHit, 1000f ) ) {
+			// now, if player clicks and NOT on an NPC, then instantiate a new NPC
+			if ( Input.GetMouseButtonDown (0) && rayHit.collider.tag != "Player" ) {
+				Vector3 spawnPosition = rayHit.point + rayHit.normal;
+				// if you want to REMEMBER the clone you instantiate, make sure you cast the type ("as Raycast") at the end
+				NPCRaycast newNPC = Instantiate ( npcPrefab, spawnPosition, Quaternion.Euler (0, 0, 0) ) as NPCRaycast;
+				allMyNpcs.Add ( newNPC ); // add the clone to the list of NPCs (see above)
+			}
+		}
+
+		// if we press R, then go through the entire list and multiply each NPC's speed by 2
+		if ( Input.GetKeyDown (KeyCode.R) ) {
+			// "var" infers the type based on available information. In this case, we already know allMyNpcs is a list of NPCRaycast objects
+			foreach ( var thisNPC in allMyNpcs ) { // "foreach" is a shortcut "for" loop designed for iterating through arrays and lists
+				thisNPC.speed *= 2f; // we can access "speed" from this script because it is a public variable in NPCRaycast.cs
+			}
+		}
+
+
+	}
+}
+
+
---
+++ week10/Assets/scripts/NPCRaycast.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// put this on a Capsule with a Rigidbody
+public class NPCRaycast : MonoBehaviour {
+
+	public float speed = 0.5f;
+
+	// FixedUpdate is called once per physics frame / fixed timestep
+	void FixedUpdate () {
+
+		// always move forward
+		GetComponent<Rigidbody>().AddForce ( transform.forward * speed, ForceMode.VelocityChange );
+
+		// generate ray
+		Ray ray = new Ray( transform.position, transform.forward );
+
+		// actually shoot raycast
+		if ( Physics.Raycast ( ray, 3f ) ) {
+			// if the raycast hits something, then rotate
+			transform.Rotate ( 0f, Random.Range (-90f, 90f), 0f );
+		}
+
+	}
+}
